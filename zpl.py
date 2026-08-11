6bc53af6061b9ebe9bb29467e754cb734b16f252
Jose Vasquez <soportewms@panafoto.local>
2026-08-11 12:00:29 -0500

    ajuste de codigo de barra

--- zpl.py
+++ zpl.py
@@ -6,11 +6,14 @@ def generate_barcode_label(barcode: str) -> str:
 ^LL203
 ^LH0,0
 
-^FO180,45^BY3,2,80
+^FO180,100
+^BY3,2,80
 ^BCN,80,N,N,N
-^FD{barcode}^FS
+^FD>;{barcode}^FS
 
-^FO0,130^A0N,36,36^FB710,1,0,C,0
+^FO0,200
+^A0N,36,36
+^FB710,1,0,C,0
 ^FD{barcode}^FS
 
 ^XZ
